ba5e5f7c461c62425ee6a631b69bd43f4cf38bdc
Deltager <teknisk+GDC2024-GR8@game.unf.dk>
2024-07-14 23:37:16 +0200

    Made some sound bools

--- TheUnityProject/Assets/Script/Player/PlayerMovement.cs
+++ TheUnityProject/Assets/Script/Player/PlayerMovement.cs
@@ -22,6 +22,9 @@ public class PlayerMovement : MonoBehaviour
     public bool jumpEnable = true;
     
     private Vector3 jumpVector;
+
+    public bool PLAYSOUND_Running;
+    public bool PLAYSOUND_Walking;
     
     void Start()
     {
@@ -30,21 +33,23 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        /*
-        isRunning = Input.GetKey(KeyCode.LeftShift);
-
-        if (isRunning) speed = runSpeed;
-        else speed = walkSpeed;
         
-        // PLayer shuold move using WASD
-        input = rb.velocity;
-        
-        input.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime * 250;
-        input.z = Input.GetAxis("Vertical") * speed * Time.deltaTime * 250;
-
-        rb.velocity = input;
-        */
-
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            if (isRunning)
+            {
+                PLAYSOUND_Running = isRunning;
+            }
+            else
+            {
+                PLAYSOUND_Walking = isWalking;
+            }
+        }
+        else
+        {
+            PLAYSOUND_Walking = isWalking;
+            PLAYSOUND_Running = isRunning;
+        }
         playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         
         jumpVector = new Vector3(playerInput.x, jumpHeight, playerInput.z);
@@ -55,12 +60,13 @@ public class PlayerMovement : MonoBehaviour
             Jump();
             jumpEnable = false;
         }
+        Debug.Log("Run: " + PLAYSOUND_Running);
+        Debug.Log("Walk: " + PLAYSOUND_Walking);
 
     }
 
     private void FixedUpdate()
     {
-        
         if (playerInput.magnitude > 1)
         {
             playerInput = playerInput.normalized;
@@ -68,15 +74,32 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 moveVector = transform.TransformDirection(playerInput);
 
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift) && (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0))
         {
             isRunning = true;
             isWalking = false;
+            PLAYSOUND_Running = true;
+            PLAYSOUND_Walking = false;
+        }
+        else if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            isRunning = false;
+            isWalking = true;
+            PLAYSOUND_Running = false;
+            PLAYSOUND_Walking = true;
+        }
+        else
+        {
+            isRunning = false;
+            isWalking = false;
+            PLAYSOUND_Running = false;
+            PLAYSOUND_Walking = false;
         }
         if (isRunning) speed = runSpeed;
         else speed = walkSpeed;
 
         controller.SimpleMove( speed * 250 * moveVector);
+        
     }
 
     private void Jump()
