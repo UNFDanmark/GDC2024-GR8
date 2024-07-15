891bd42dd815407930f20ab8f2b591e954bcce06
Deltager <teknisk+GDC2024-GR8@game.unf.dk>
2024-07-15 11:31:43 +0200

    tis code is horrible but thats fine

--- TheUnityProject/Assets/Script/AudioController.cs
+++ TheUnityProject/Assets/Script/AudioController.cs
@@ -6,25 +6,91 @@ public class AudioController : MonoBehaviour
 {
     private PlayerMovement playerMovement;
     [SerializeField] private GameObject PlayerObject;
+    private bool WalkSoundHasPlayed = false;
+    private CharacterController PlayerController;
+    public AudioClip[] clipList;
+    // 1. Footstep 
+    // 2. Footstep
+    // 3. Footstep
 
     private AudioSource WalkingAudio;
     // Start is called before the first frame update
     void Start()
     {
-        playerMovement = GetComponent<PlayerMovement>();
+        // some of this is not used
+      
         PlayerObject = GameObject.FindWithTag("Player");
+        playerMovement = PlayerObject.GetComponent<PlayerMovement>();
         WalkingAudio = PlayerObject.GetComponent<AudioSource>();
+        PlayerController = PlayerObject.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
 
-        if (PlayerObject.GetComponent<PlayerMovement>().PLAYSOUND_Walking || )
+        
+        if (playerMovement.isRunning)
+        {
+            WalkingAudio.pitch = Random.Range(1.6f, 1.8f); // pitch for variation
+            WalkingAudio.volume = Random.Range(0.07f, 0.09f); // Volume for Variation 
+        }
+        else
+        {
+            WalkingAudio.pitch = Random.Range(1.2f, 1.4f); // pitch for variation
+            WalkingAudio.volume = Random.Range(0.03f, 0.05f); // Volume for Variation 
+        }
+        
+        if (PlayerController.velocity.z != 0 && PlayerController.velocity.x != 0 && WalkSoundHasPlayed == false)
+        {
+
+            WalkingAudio.clip = clipList[Random.Range(0, clipList.Length)]; 
+            // Inserts a random clip from list into player audio source
+            
+            print("paasdsadsadasd");
+
+
+            if (playerMovement.isWalking)
+            {
+                WalkingAudio.Play();
+                print("pluh");
+                WalkSoundHasPlayed = true;
+
+            }
+            else
+            {
+                WalkingAudio.Play();
+                print("pluh");
+                WalkSoundHasPlayed = true;
+            }
+            
+          
+            
+            
+        }
+        else if (PlayerController.velocity.z == 0 || PlayerController.velocity.x == 0)
+        {
+            WalkingAudio.Stop();
+            WalkSoundHasPlayed = false;
+        }
+        {
+          
+        }
+
+        
+        
+        
+/*
+        if (PlayerObject.GetComponent<PlayerMovement>().PLAYSOUND_Walking && WalkSoundHasPlayed == false )
         {
             WalkingAudio.Play();
             print("ur walki22n");
-        } 
+            WalkSoundHasPlayed = true;
+        }
+        else
+        {
+            WalkingAudio.Stop();
+            WalkSoundHasPlayed = false;
+        } */
     }
 }
