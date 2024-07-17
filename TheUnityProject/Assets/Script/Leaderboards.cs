1b2bab33f94edd9a840249e544dcf2415568f27f
Deltager <teknisk+GDC2024-GR8@game.unf.dk>
2024-07-17 22:51:07 +0200

    audio sfx

--- TheUnityProject/Assets/Script/Leaderboards.cs
+++ TheUnityProject/Assets/Script/Leaderboards.cs
@@ -5,23 +5,37 @@ using UnityEngine;
 public class Leaderboards : MonoBehaviour
 {
     public int[] topScores;
-
     public string[] topNames;
+    public string inputName;
 
     private PlayerHealth ph;
+    private bool dieBool, testbool;
     public Score sc;
 
     public int topWhat = 10;
-    // Start is called before the first frame update
+
+    public void NewHighScore(int position)
+    {
+        topNames[position] = inputName;
+    }
     public void SaveData()
     {
         Debug.Log("Called save data");
         for (int i = 0; i < topWhat; i++)
         {
-            Debug.Log("Made it to the for loop");
             PlayerPrefs.SetInt("#"+(i+1),topScores[i]);
-            Debug.Log("Saved value #"+(i+1)+" to " + topScores[i]);
             PlayerPrefs.SetString("#"+(i+1),topNames[i]);
+        }
+        PlayerPrefs.Save();
+    }
+
+    public void ClearData()
+    {
+        for (int i = 0; i < topWhat; i++)
+        {
+            topScores[i] = 0;
+            topNames[i] = "";
+            SaveData();
         }
     }
 
@@ -39,6 +53,7 @@ public class Leaderboards : MonoBehaviour
     }
     void Start()
     {
+        dieBool = true;
         LoadData();
         ph = gameObject.GetComponent<PlayerHealth>();
     }
@@ -46,6 +61,8 @@ public class Leaderboards : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.P)) ClearData();
+        if (Input.GetKeyDown(KeyCode.I)) testbool = true;
         if (Input.GetKeyDown(KeyCode.O))
         {
             for (int i = 0; i < topWhat; i++)
@@ -53,23 +70,36 @@ public class Leaderboards : MonoBehaviour
                 Debug.Log($"#{i+1}:{topScores[i]}");
             }
         }
-        if (ph.playerDied)
+        if ((ph.playerDied && dieBool)|| testbool)
         {
-            Debug.Log("PLAYER HAS DIED!!!!");
+            dieBool = false; testbool = false;
             int tempScore = (int)sc.score;
+            
             for (int i = topWhat-1; i > 0; i--)
             {
-                Debug.Log(i);
                 if (tempScore > topScores[i] && tempScore < topScores[i-1])
                 {
+                    MoveScoreDown(i);
                     topScores[i] = tempScore;
+                    NewHighScore(i);
                 }
 
-                if (tempScore > topScores[0]) topScores[0] = tempScore;
+                if (tempScore > topScores[0])
+                {
+                    MoveScoreDown(0);
+                    topScores[0] = tempScore;
+                    NewHighScore(0);
+                }
             }
-
-            topScores[1] = 298428794;
             SaveData();
+        }
+    }
+
+    void MoveScoreDown(int index)
+    {
+        for (int i = topWhat-1; i > index; i--)
+        {
+            topScores[i] = topScores[i - 1];
         }
     }
 }
