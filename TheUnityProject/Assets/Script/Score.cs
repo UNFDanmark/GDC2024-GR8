443cea0a2cf92e823cb3fc81c330c41e9a4b9552
Deltager <teknisk+GDC2024-GR8@game.unf.dk>
2024-07-18 00:24:02 +0200

    LeaderboardLatest

--- TheUnityProject/Assets/Script/Score.cs
+++ TheUnityProject/Assets/Script/Score.cs
@@ -8,6 +8,7 @@ public class Score : MonoBehaviour
     private TMP_Text textComponent;
     public Killstreak killstreak;
     public Transform playerTransform;
+    public Leaderboards lb;
 
     public float score = 0;
     public float scoreMultiplier = 1;
@@ -22,6 +23,7 @@ public class Score : MonoBehaviour
     {
         scoreMultiplier = playerTransform.position.y * 0.25f * (1 + killstreak.killStreak * 0.1f)+.75f;
         textComponent.text = $"Score: {score:0000000}\n" +
-                             $"Multiplier: {scoreMultiplier:F1}x";
+                             $"Multiplier: {scoreMultiplier:F1}x" +
+                             $"HighScore: {PlayerPrefs.GetInt("#1",1)}";
     }
 }
